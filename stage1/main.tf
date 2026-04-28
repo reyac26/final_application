7d881687a57a4016aa2d86dabf8d86315889ee18
Reya <nereydacastro15@gmail.com>
2026-04-28 02:18:26 -0500

    Add Terraform configuration for Azure resources

---
+++ stage1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "nereydacastro-ACR" {
+  name = "nereydacastroacmp2400"
+  resource_group_name = "rg-nereydacastro"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
